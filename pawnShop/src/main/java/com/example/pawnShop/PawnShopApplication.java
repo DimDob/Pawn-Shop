c8250407f88a27c4b8c6b3528d7fa55b5c6da25b
DimDob <dimitardobrev.business@gmail.com>
2024-05-21 22:56:18 +0300

    init repo

---
+++ pawnShop/src/main/java/com/example/pawnShop/PawnShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.pawnShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawnShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawnShopApplication.class, args);
+	}
+
+}
---
+++ pawnShop/src/test/java/com/example/pawnShop/PawnShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pawnShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawnShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
