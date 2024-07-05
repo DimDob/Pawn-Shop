69769a09a9730e5f124d905b2e842cad2dd31c57
Todor Todorov <t.todorov135@gmail.com>
2024-07-05 09:21:33 +0300

    add try-chatch block to auth service

--- pawnShop/src/main/java/com/example/pawnShop/Service/AuthServiceImp.java
+++ pawnShop/src/main/java/com/example/pawnShop/Service/AuthServiceImp.java
@@ -3,6 +3,7 @@ package com.example.pawnShop.Service;
 import com.example.pawnShop.Dto.Auth.LoginRequestDto;
 import com.example.pawnShop.Dto.Auth.LoginResponseDto;
 import com.example.pawnShop.Dto.Auth.RegisterRequestDto;
+import com.example.pawnShop.Dto.Result;
 import com.example.pawnShop.Entity.AppUser;
 import com.example.pawnShop.Factory.Contract.AuthFactory;
 import com.example.pawnShop.Repository.UserRepository;
@@ -36,47 +37,56 @@ public class AuthServiceImp implements AuthService {
     private final JwtService jwtService;
 
     @Override
-    public LoginResponseDto login(LoginRequestDto loginRequestDto) {
+    public Result<LoginResponseDto> login(LoginRequestDto loginRequestDto) {
         try {
             if (loginRequestDto == null) {
-                return null;
+                return Result.error("The fields are empty.");
             }
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(loginRequestDto.getEmail(), loginRequestDto.getPassword()));
+            if(!authentication.isAuthenticated()){
+               return  Result.error("Incorrect email or password.");
+            }
 
             SecurityContextHolder.getContext().setAuthentication(authentication);
             AppUser user = (AppUser) authentication.getPrincipal();
 
-            return LoginResponseDto.builder()
+            return Result.success(LoginResponseDto.builder()
                     .username(user.getEmail())
                     .token(jwtService.generateJwtToken(user))
                     .isAdmin(user.getIsAdmin())
-                    .build();
+                    .build());
 
         } catch (AuthenticationException e){
-            return  null;
+
+            return  Result.error("Something went wrong");
         }
     }
 
     @Override
-    public boolean register(RegisterRequestDto registerRequestDto) {
-        if(registerRequestDto == null){
-            return false;
-        }
-        if(!registerRequestDto.getPassword().equals(registerRequestDto.getConfirmPassword())){
-            return false;
-        }
-        Optional<AppUser> user = userRepository.findByEmail(registerRequestDto.getEmail());
+    public Result<Boolean> register(RegisterRequestDto registerRequestDto) {
+        try {
+            if (registerRequestDto == null) {
+                return Result.error("The fields are empty.");
+            }
+            if (!registerRequestDto.getPassword().equals(registerRequestDto.getConfirmPassword())) {
+                return Result.error("Password and confirmed password do not match.");
+            }
+            Optional<AppUser> user = userRepository.findByEmail(registerRequestDto.getEmail());
 
-        if(user.isEmpty()){
+            if (!user.isEmpty()) {
+                return Result.error("There is a user with this email.");
+            }
             String encodedPassword = passwordEncoder.encode(registerRequestDto.getPassword());
             AppUser newUser = authFactory.createUser(registerRequestDto, encodedPassword);
 
             userRepository.save(newUser);
 
-            return true;
-        }
+            return Result.success(true);
 
-        return false;
+        } catch(Exception e){
+
+            return Result.error("You can not register this user.");
+        }
     }
 }
--- pawnShop/src/main/java/com/example/pawnShop/Service/Contract/AuthService.java
+++ pawnShop/src/main/java/com/example/pawnShop/Service/Contract/AuthService.java
@@ -3,8 +3,9 @@ package com.example.pawnShop.Service.Contract;
 import com.example.pawnShop.Dto.Auth.LoginRequestDto;
 import com.example.pawnShop.Dto.Auth.LoginResponseDto;
 import com.example.pawnShop.Dto.Auth.RegisterRequestDto;
+import com.example.pawnShop.Dto.Result;
 
 public interface AuthService {
-    public LoginResponseDto login(LoginRequestDto loginRequestDto);
-    public boolean register(RegisterRequestDto registerRequestDto);
+    public Result<LoginResponseDto> login(LoginRequestDto loginRequestDto);
+    public Result<Boolean> register(RegisterRequestDto registerRequestDto);
 }
