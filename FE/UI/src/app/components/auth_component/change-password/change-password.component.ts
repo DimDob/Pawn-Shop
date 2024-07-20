27985361f340b73f31aa43e3fdf55f400dba7cfb
Todor Mladenov <toshko958@gmail.com>
2024-07-20 19:19:38 +0200

    Add change password functionality

--- FE/UI/src/app/components/auth_component/change-password/change-password.component.ts
+++ FE/UI/src/app/components/auth_component/change-password/change-password.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../../../app.service';
+import { PrismData } from '../login/login_interfaces.ts/prismData';
+import prismDetailsTemplate from '../login/templates/prismDetails.template';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
   styleUrl: './change-password.component.scss'
 })
-export class ChangePasswordComponent {
+export class ChangePasswordComponent implements OnInit {
+  private endpoint: string = 'http://localhost:8080/auth/change-password';
 
+  public prismDetails: PrismData;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.prismDetails = { ...prismDetailsTemplate }
+  }
+
+  onSubmitHandler(resetPasswordForm: NgForm) {
+    if (resetPasswordForm.invalid) {
+      return
+    }
+    
+    this.authService.handlerChangePassword(this.prismDetails, this.endpoint);
+  }
 }
