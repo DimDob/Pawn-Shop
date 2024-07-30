ce8fb30def977ae02ccfddf6ad5abe54eedf52ef
Savina Popova <savina.popova@yahoo.com>
2024-07-30 17:32:16 +0300

    seed 120 products

--- pawnShop/src/main/java/com/example/pawnShop/DataSeeders/DataSeeder.java
+++ pawnShop/src/main/java/com/example/pawnShop/DataSeeders/DataSeeder.java
@@ -2,8 +2,6 @@ package com.example.pawnShop.DataSeeders;
 
 import com.example.pawnShop.Entity.*;
 import com.example.pawnShop.Repository.*;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Sort;
@@ -17,30 +15,53 @@ import java.util.List;
 import java.util.UUID;
 
 @Component
-@RequiredArgsConstructor
 public class DataSeeder implements CommandLineRunner {
 
-    @Autowired
+
     private final CityRepository cityRepository;
-    @Autowired
+
     private final AddressRepository addressRepository;
-    @Autowired
+
     private final PasswordEncoder passwordEncoder;
-    @Autowired
+
     private final UserRepository userRepository;
-    @Autowired
+
     private final PaymentTypeRepository paymentTypeRepository;
-    @Autowired
+
     private final PaymentRepository paymentRepository;
-    @Autowired
+
     private final PawnShopRepository pawnShopRepository;
+
+    private final ProductRepository productRepository;
+
+    private final ProductTypeRepository productTypeRepository;
+
+    public DataSeeder(CityRepository cityRepository, AddressRepository addressRepository,
+                      PasswordEncoder passwordEncoder, UserRepository userRepository,
+                      PaymentTypeRepository paymentTypeRepository, PaymentRepository paymentRepository,
+                      PawnShopRepository pawnShopRepository, ProductRepository productRepository, ProductTypeRepository productTypeRepository) {
+        this.cityRepository = cityRepository;
+        this.addressRepository = addressRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+        this.paymentTypeRepository = paymentTypeRepository;
+        this.paymentRepository = paymentRepository;
+        this.pawnShopRepository = pawnShopRepository;
+        this.productRepository = productRepository;
+        this.productTypeRepository = productTypeRepository;
+    }
 
     @Override
     public void run(String... args) throws Exception {
+        setProductTypes();
+        setProducts();
     }
 
     @Bean
     private void setCities(){
+        if (this.cityRepository.count() != 0) {
+            return;
+        }
         List<City> cities = new ArrayList<>();
         City sofia =  City.builder()
                 .id(UUID.fromString("0a5fb26f-e1fd-421f-a7c7-1ab88fb43d49"))
@@ -59,6 +80,9 @@ public class DataSeeder implements CommandLineRunner {
     }
     @Bean
     private void setAddresses(){
+        if (this.addressRepository.count() != 0) {
+            return;
+        }
       List<City> cities = cityRepository.findAll(Sort.by("name"));
 
       List<Address> addresses = new ArrayList<>();
@@ -83,6 +107,9 @@ public class DataSeeder implements CommandLineRunner {
   }
     @Bean
     private void setUsers(){
+        if (this.userRepository.count() != 0) {
+            return;
+        }
         List<AppUser> users = new ArrayList<>();
 
         AppUser superAdmin = new AppUser();
@@ -136,6 +163,9 @@ public class DataSeeder implements CommandLineRunner {
     }
     @Bean
     private void setPaymentTypes(){
+        if (this.paymentTypeRepository.count() != 0) {
+            return;
+        }
         List<PaymentType> paymentTypes = new ArrayList<>();
 
         PaymentType monthly = PaymentType.builder()
@@ -168,6 +198,9 @@ public class DataSeeder implements CommandLineRunner {
     }
     @Bean
     private void setPayments(){
+        if (this.paymentRepository.count() != 0) {
+            return;
+        }
         List<Payment> payments = new ArrayList<>();
 
         List<PaymentType> paymentTypes = paymentTypeRepository.findAll();
@@ -194,6 +227,9 @@ public class DataSeeder implements CommandLineRunner {
     }
     @Bean
     private void setPawnShop(){
+        if (this.pawnShopRepository.count() != 0) {
+            return;
+        }
         List<Address> addresses = addressRepository.findAll(Sort.by(Sort.Direction.ASC, "name"));
         List<AppUser> users = userRepository.findAll();
         List<Payment> payments = paymentRepository.findAll();
@@ -230,5 +266,63 @@ public class DataSeeder implements CommandLineRunner {
         pawnShops.add(secondPawnShop);
 
         pawnShopRepository.saveAll(pawnShops);
+    }
+
+    private void setProductTypes() {
+        if (this.productTypeRepository.count() != 0) {
+            return;
+        }
+        List<ProductType> productTypes = new ArrayList<>();
+        productTypes.add(new ProductType(UUID.fromString("2a6ae85c-aeee-4452-a58b-ce54d801daef"), "Телефон"));
+        productTypes.add(new ProductType(UUID.fromString("7698cfb6-697d-409b-a7e4-dc21fa621647"), "Таблет"));
+        productTypes.add(new ProductType(UUID.fromString("7d853cb2-d59a-46ca-905a-09948ac3fc51"), "Телевизор"));
+        productTypes.add(new ProductType(UUID.fromString("a3e82346-3395-4b9c-842d-14973a83fa99"), "Аудио"));
+        productTypes.add(new ProductType(UUID.fromString("e8f6c58c-bb2d-455c-aa1c-6b9f3941c034"), "Електроника"));
+        productTypes.add(new ProductType(UUID.fromString("b5c2d8b9-7b88-4e9f-9729-83f3f9551680"), "Лаптоп"));
+        productTypes.add(new ProductType(UUID.fromString("92e84a89-d806-4b86-bf69-f0e07eeef872"), "Компютър"));
+        productTypes.add(new ProductType(UUID.fromString("7abc38a4-1ac3-4334-ac17-bdafec696987"), "Компютърна периферия"));
+        productTypes.add(new ProductType(UUID.fromString("8a4ed01c-3101-47d8-ac88-de18febf3737"), "Домакински електроуред"));
+        productTypes.add(new ProductType(UUID.fromString("801b09f8-bf92-494e-941e-7c9fe54ef25a"), "Малък електроуред"));
+        productTypes.add(new ProductType(UUID.fromString("c4b52ba5-462a-48fb-b9e5-d38a53c0c1ab"), "Уред за здраве и красота"));
+        productTypes.add(new ProductType(UUID.fromString("55a9017d-bcb4-4e3d-88b1-62970ae6fc4b"), "Фото и видео"));
+        productTypes.add(new ProductType(UUID.fromString("0fe76761-f043-468e-b14f-70ffbec3b6a9"), "Автомобилен аксесоар"));
+        productTypes.add(new ProductType(UUID.fromString("99bef4ec-767b-4833-a6f5-ce387b7499f1"), "Злато"));
+        productTypes.add(new ProductType(UUID.fromString("cc182299-4aa8-4d12-98e9-5a670b757e80"), "Сребро"));
+        productTypes.add(new ProductType(UUID.fromString("21683274-b521-44d4-b396-47077e348377"), "Бижу"));
+
+        int dbCountOfProductTypes = productTypeRepository.getCountOfRecords();
+
+        if(dbCountOfProductTypes <= 0){
+            productTypeRepository.saveAll(productTypes);
+        }
+    }
+
+
+    private void setProducts() {
+        if (this.productRepository.count() != 0) {
+            return;
+        }
+        List<Product> products = new ArrayList<>();
+
+        for (int i = 1; i <= 120; i++) {
+
+
+            List<ProductType> productTypes = this.productTypeRepository.findAll();
+            ProductType productType = productTypes.get(i % productTypes.size());
+
+            Product product = Product.builder()
+                    .id(UUID.randomUUID())
+                    .name("Product " + i)
+                    .productType(productType)
+                    .marketPrice(BigDecimal.valueOf(100 + i))
+                    .pawnPercentage(BigDecimal.valueOf(0.1 * i))
+                    .secondHandPrice(BigDecimal.valueOf(50 + i))
+                    .pictureUrl("http://example.com/product" + i + ".jpg")
+                    .build();
+            System.out.println(product);
+            products.add(product);
+        }
+
+        productRepository.saveAll(products);
     }
 }
