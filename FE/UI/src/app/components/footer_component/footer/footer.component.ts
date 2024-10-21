a1d49778f9426fab8d372369d605337d939972f4
Georgi Markov <gogata1905@gmail.com>
2024-10-21 04:02:54 +0300

    add footer

---
+++ FE/UI/src/app/components/footer_component/footer/footer.component.ts
@@ -0,0 +1,9 @@
+// UI\src\app\components\footer_component\footer\footer.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.scss"]
+})
+export class FooterComponent {}
