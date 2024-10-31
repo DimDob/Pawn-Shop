1088a8e574d22c7601517c29a6325019336c6ce5
Georgi Markov <gogata1905@gmail.com>
2024-10-31 11:42:11 +0200

    update product interface

--- FE/UI/src/app/components/main_page_component/main-page/Interfaces/Products.ts
+++ FE/UI/src/app/components/main_page_component/main-page/Interfaces/Products.ts
@@ -1,4 +1,5 @@
 // UI\src\app\components\main_page_component\main-page\Interfaces\Products.ts
+import { Category } from "../enums/Category";
 export interface Products {
   id: number;
   picture: string;
@@ -10,6 +11,21 @@ export interface Products {
   name: string;
   category: string;
   price: number;
-  //quantityInStock: number; // add this later // not now
   ownerId?: number;
 }
+
+// export interface Products {
+//   id: string;
+//   picture: string;
+//   color?: string;
+//   size?: number;
+//   sex?: "male" | "female" | "unisex";
+//   manufacturer?: string;
+//   model?: string;
+//   name: string;
+//   category: Category;
+//   price: number;
+//   quantityInStock?: number;
+//   isRunOutOfStock?: boolean;
+//   ownerId?: string;
+// }
