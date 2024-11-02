80a22a8a2a55b2b1d8fc3d81974cee32e30b4dc4
Georgi Markov <gogata1905@gmail.com>
2024-11-02 19:53:38 +0200

    register components - ng18

--- FE/UI/src/app/components/auth_component/register/register.component.ts
+++ FE/UI/src/app/components/auth_component/register/register.component.ts
@@ -1,8 +1,8 @@
 // UI\src\app\components\auth_component\register\register.component.ts
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { PrismData } from "../login/login_interfaces.ts/prismData";
-import prismDetailsTemplate from "../login/templates/prismDetails.template";
 import { NgForm } from "@angular/forms";
+import { signal } from "@angular/core";
 
 @Component({
   selector: "app-register",
@@ -10,14 +10,24 @@ import { NgForm } from "@angular/forms";
   styleUrls: ["./register.component.scss"]
 })
 export class RegisterComponent {
-  @Input() prismDetails: PrismData;
-  @Output() userRegister: EventEmitter<PrismData> = new EventEmitter<PrismData>();
+  @Input() public prismDetails!: PrismData;
+  @Output() private userRegister = new EventEmitter<PrismData>();
 
-  createAccount(createAccountForm: NgForm) {
-    if (createAccountForm.invalid) {
+  private isSubmitting = signal(false);
+
+  public createAccount(createAccountForm: NgForm): void {
+    if (createAccountForm.invalid || this.isSubmitting()) {
+      alert("Please fix the form errors before submitting");
       return;
     }
 
-    this.userRegister.emit(this.prismDetails);
+    try {
+      this.isSubmitting.set(true);
+      this.userRegister.emit(this.prismDetails);
+    } catch (error) {
+      alert("An error occurred while creating your account: " + error);
+    } finally {
+      this.isSubmitting.set(false);
+    }
   }
 }
