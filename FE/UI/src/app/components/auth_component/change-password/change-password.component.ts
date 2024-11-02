5076495081e509c930bb6bd660453fabbd391bb2
Georgi Markov <gogata1905@gmail.com>
2024-11-02 15:01:15 +0200

    change password components to ng18

--- FE/UI/src/app/components/auth_component/change-password/change-password.component.ts
+++ FE/UI/src/app/components/auth_component/change-password/change-password.component.ts
@@ -1,6 +1,6 @@
 // UI\src\app\components\auth_component\change-password\change-password.component.ts
-import { Component, OnInit } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { Component, OnInit, OnDestroy, signal } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { PrismData } from "../login/login_interfaces.ts/prismData";
 import prismDetailsTemplate from "../login/templates/prismDetails.template";
 import { ChangePasswordService } from "./change-password.service";
@@ -12,31 +12,49 @@ import { ActivatedRoute } from "@angular/router";
   templateUrl: "./change-password.component.html",
   styleUrl: "./change-password.component.scss"
 })
-export class ChangePasswordComponent implements OnInit {
-  public prismDetails: PrismData;
+export class ChangePasswordComponent implements OnInit, OnDestroy {
+  resetPasswordForm: FormGroup;
+  prismDetails = signal<PrismData>({ ...prismDetailsTemplate });
+  userId = signal<string | null>(null);
+  private changePasswordSubscription?: Subscription;
 
-  public userId: string | null;
-
-  public subscription: Subscription;
-
-  public changePasswordSubscription: Subscription;
-
-  constructor(private changePasswordService: ChangePasswordService, private activeRoute: ActivatedRoute) {}
-
-  ngOnDestroy(): void {
-    this.changePasswordSubscription.unsubscribe();
+  constructor(
+    private fb: FormBuilder,
+    private changePasswordService: ChangePasswordService,
+    private activeRoute: ActivatedRoute
+  ) {
+    this.resetPasswordForm = this.fb.group({
+      password: ['', Validators.required],
+      reEnteredPassword: ['', Validators.required]
+    }, { validator: this.passwordMatchValidator });
   }
 
   ngOnInit(): void {
-    this.prismDetails = { ...prismDetailsTemplate };
+    this.userId.set(this.activeRoute.snapshot.paramMap.get("userId"));
   }
 
-  onSubmitHandler(resetPasswordForm: NgForm): void {
-    if (resetPasswordForm.invalid) {
+  ngOnDestroy(): void {
+    this.changePasswordSubscription?.unsubscribe();
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password')?.value === g.get('reEnteredPassword')?.value
+      ? null
+      : { mismatch: true };
+  }
+
+  onSubmit(): void {
+    if (this.resetPasswordForm.invalid) {
       return;
     }
 
-    this.userId = this.activeRoute.snapshot.paramMap.get("userId");
-    this.changePasswordSubscription = this.changePasswordService.changePassword(this.prismDetails, this.userId);
+    const passwordData = {
+      ...this.prismDetails(),
+      changedPassword: this.resetPasswordForm.get('password')?.value,
+      changedPassword2: this.resetPasswordForm.get('reEnteredPassword')?.value
+    };
+
+    this.changePasswordSubscription = this.changePasswordService
+      .changePassword(passwordData, this.userId());
   }
 }
