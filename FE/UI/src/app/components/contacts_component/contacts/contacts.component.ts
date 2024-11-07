3ab3b8c5c72ffd884c438e4a736609f64ac5e5b2
Georgi Markov <gogata1905@gmail.com>
2024-11-07 03:00:19 +0200

    add good UI for contacts

--- FE/UI/src/app/components/contacts_component/contacts/contacts.component.ts
+++ FE/UI/src/app/components/contacts_component/contacts/contacts.component.ts
@@ -1,9 +1,18 @@
 // UI\src\app\components\contacts_component\contacts\contacts.component.ts
 import { Component } from "@angular/core";
+import { 
+  faEnvelope,
+  faPhone,
+  faLocationDot
+} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: "app-contacts",
   templateUrl: "./contacts.component.html",
   styleUrls: ["./contacts.component.scss"]
 })
-export class ContactsComponent {}
+export class ContactsComponent {
+  faEnvelope = faEnvelope;
+  faPhone = faPhone;
+  faLocationDot = faLocationDot;
+}
