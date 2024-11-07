3cd591f18d61b0f25796a078be0153342a483ab0
Georgi Markov <gogata1905@gmail.com>
2024-11-08 01:23:00 +0200

    success page updated UI

--- FE/UI/src/app/components/success_page_component/success-page/success-page.component.ts
+++ FE/UI/src/app/components/success_page_component/success-page/success-page.component.ts
@@ -3,6 +3,11 @@
 import { Component, OnInit } from "@angular/core";
 import { CartService } from "../../cart_page_component/cart-page/cart.service";
 import { NotificationService } from "../../../shared/services/notification.service";
+import {
+  faCheckCircle,
+  faHome,
+  faShoppingBag
+} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: "app-success-page",
@@ -10,6 +15,10 @@ import { NotificationService } from "../../../shared/services/notification.servi
   styleUrls: ["./success-page.component.scss"]
 })
 export class SuccessPageComponent implements OnInit {
+  faCheckCircle = faCheckCircle;
+  faHome = faHome;
+  faShoppingBag = faShoppingBag;
+
   constructor(
     private cartService: CartService,
     private notificationService: NotificationService
