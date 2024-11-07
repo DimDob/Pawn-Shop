8ce8edf052569411afaf27c78981e90b8e537fba
Georgi Markov <gogata1905@gmail.com>
2024-11-07 03:50:26 +0200

    change notifications to 1s long

--- FE/UI/src/app/shared/services/notification.service.ts
+++ FE/UI/src/app/shared/services/notification.service.ts
@@ -7,7 +7,7 @@ import { MatSnackBar, MatSnackBarConfig } from "@angular/material/snack-bar";
 })
 export class NotificationService {
   private config: MatSnackBarConfig = {
-    duration: 3000,
+    duration: 1000,
     horizontalPosition: "right",
     verticalPosition: "top"
   };
