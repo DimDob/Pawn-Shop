294ffb3ada8c4a42ebb61460ce7f3677dd970e5d
Georgi Markov <gogata1905@gmail.com>
2024-12-09 01:04:19 +0200

    update vercel

--- FE/UI/vercel-build.sh
+++ FE/UI/vercel-build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export NODE_OPTIONS="--max_old_space_size=4096"
+npm install -g @angular/cli
 npm install --legacy-peer-deps
-npm run build
-cp dist/browser/index.html dist/browser/404.html 
+ng build --configuration production
+cp dist/browser/index.html dist/browser/404.html
